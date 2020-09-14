279fcae73db4a14b63cde3f3f45497b432550205
Michael Cinus <michaelcinus@gmail.com>
2020-09-14 17:58:33 +0200

    **

--- select.php
+++ select.php
@@ -1,7 +1,7 @@
 <?php
     $servername = "localhost";
-    $username = "guybrush";
-    $password = "code";
+    $username = "root";
+    $password = "root";
     $dbname = "dbhotel";
     $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -11,7 +11,41 @@
         return;
         
     }
-    $sql = "
+
+     //seleziona dalla tabella pagamenti le colonne id, status e price di tutti i pagamenti con price superiore a 600, stampa il risultato in una lista non ordinata
+    /*$sql = "
+
+        SELECT pagamenti.id, pagamenti.status, pagamenti.price
+        FROM pagamenti
+        WHERE price > 600
+
+    ";
+    
+    $result = $conn->query($sql);
+
+    if ($result && $result->num_rows > 0) {
+
+        echo '<ul>';
+       
+        while($row = $result->fetch_assoc()) {
+
+            echo $row['id'] ." - " .$row['status'] ." - " .$row['price'] . '<br>';
+            echo '</li>';
+
+        }
+
+    } elseif ($result) {
+
+        echo "0 results";
+
+    } else {
+
+        echo "query error";
+
+    }*/
+
+    //seleziona tutto dalla tabella pagamenti e stampa il risultato in una lista ordinata (fatto in classe)
+    /*$sql = "
 
         SELECT *
         FROM pagamenti
@@ -39,5 +73,6 @@
 
         echo "query error";
 
-    }
+    }*/
+
     $conn->close();
